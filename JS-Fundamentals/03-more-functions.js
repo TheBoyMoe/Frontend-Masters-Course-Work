55a7e80a3ad9abe1cdd0c57a799a28ebcfbe4364
theboymo <billfero@gmail.com>
2018-07-27 18:11:07 +0100

    implementation of from function

---
+++ JS-Fundamentals/03-more-functions.js
@@ -0,0 +1,30 @@
+var add = function(a, b = 2) {
+  // arguments will not contain defail values if that arg is not used
+  console.log(arguments);
+  return a + b;
+};
+
+add(3);
+//=> log -> 3
+//=> return -> 5
+
+// implementation before es6 default values
+add = function(a, b){
+  b = b || 2;
+  return a + b;
+};
+
+// implementation of _.from
+const _ = {};
+_.from = function(arr){
+  const array = [];
+  for(let i = 0; i < arr.length; i++){
+    array.push(arr[i]);
+  }
+  return array;
+};
+
+// Alternate implementation
+_.from = function(arr){
+  return Array.prototype.slice.call(arr);
+};
