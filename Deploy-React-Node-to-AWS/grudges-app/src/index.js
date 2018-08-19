bd2b0936be072c72032a0c49963a5bdf1ebf6fed
theboymo <billfero@gmail.com>
2018-08-19 18:21:30 +0100

    configure app to communicate with aws mobile backend

--- Deploy-React-Node-to-AWS/grudges-app/src/index.js
+++ Deploy-React-Node-to-AWS/grudges-app/src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Application from './Application';
 
+// configure app for aws
+import Amplify from 'aws-amplify'
+import configuration from './aws-exports';
+Amplify.configure(configuration);
+
 ReactDOM.render(<Application />, document.getElementById('root'));
